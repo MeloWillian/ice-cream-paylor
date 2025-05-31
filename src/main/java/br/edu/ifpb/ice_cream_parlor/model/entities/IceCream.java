a062bfb1faafffb41f5e3ad5a434caabbfad9be9
melowillian.dev <melowillian.dev@gmail.com>
2025-05-31 15:55:44 -0300

    feature: adicionando enums

---
+++ src/main/java/br/edu/ifpb/ice_cream_parlor/model/entities/IceCream.java
@@ -0,0 +1,4 @@
+package br.edu.ifpb.ice_cream_parlor.model.entities;
+
+public interface IceCream {
+}
---
+++ src/main/java/br/edu/ifpb/ice_cream_parlor/model/entities/Milkshake.java
@@ -0,0 +1,4 @@
+package br.edu.ifpb.ice_cream_parlor.model.entities;
+
+public class Milkshake {
+}
