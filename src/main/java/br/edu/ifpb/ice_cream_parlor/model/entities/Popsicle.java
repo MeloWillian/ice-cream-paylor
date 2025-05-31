fa5850066ac3b974ad623365e05e56812814b874
melowillian.dev <melowillian.dev@gmail.com>
2025-05-31 15:59:29 -0300

    feature: implementa classes concretas que implementam interface IceCream

--- src/main/java/br/edu/ifpb/ice_cream_parlor/model/entities/Popsicle.java
+++ src/main/java/br/edu/ifpb/ice_cream_parlor/model/entities/Popsicle.java
@@ -1,4 +1,42 @@
 package br.edu.ifpb.ice_cream_parlor.model.entities;
 
-public class Popsicle {
+// Classe que representa um picolé.
+public class Popsicle implements IceCream {
+    private String flavor;      // Sabor do picolé.
+    private double basePrice;   // Preço base do picolé.
+
+    public Popsicle(String flavor, double basePrice) {
+        this.flavor = flavor;
+        this.basePrice = basePrice;
+    }
+
+    @Override
+    public String getName() {
+        return "Popsicle of " + flavor; // Nome formatado do picolé.
+    }
+
+    @Override
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Delicious " + flavor + " flavored popsicle."; // Descrição do picolé.
+    }
+
+    // Getter para o sabor.
+    public String getFlavor() {
+        return flavor;
+    }
+
+    // Setter para o sabor.
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    // Setter para o preço base.
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
 }
