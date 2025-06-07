e32cef0a73f2c6430eb99fb2ede00810d5ab4130
JerfesonGregorio <pro.jerfeson@gmail.com>
2025-06-07 13:08:29 -0300

    cria entidade cliente

---
+++ src/main/java/br/edu/ifpb/ice_cream_parlor/model/entities/Client.java
@@ -0,0 +1,22 @@
+package br.edu.ifpb.ice_cream_parlor.model.entities;
+
+import java.util.UUID;
+
+public class Client {
+
+    private String id;
+    private String name;
+
+    public Client(String name) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
