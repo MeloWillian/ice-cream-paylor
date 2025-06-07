2db731cd832ab3fed5a5995be715e69159f81084
JerfesonGregorio <pro.jerfeson@gmail.com>
2025-06-07 13:16:06 -0300

    remove comentario

--- src/main/java/br/edu/ifpb/ice_cream_parlor/model/entities/OrderItem.java
+++ src/main/java/br/edu/ifpb/ice_cream_parlor/model/entities/OrderItem.java
@@ -2,10 +2,9 @@ package br.edu.ifpb.ice_cream_parlor.model.entities;
 
 import br.edu.ifpb.ice_cream_parlor.patterns.decorator.IceCream;
 
-// Representa um item individual dentro de um pedido.
 public class OrderItem {
-    private IceCream iceCream; // O sorvete deste item.
-    private int quantity;      // Quantidade deste sorvete.
+    private IceCream iceCream;
+    private int quantity;
 
     public OrderItem(IceCream iceCream, int quantity) {
         if (iceCream == null) {
@@ -18,22 +17,18 @@ public class OrderItem {
         this.quantity = quantity;
     }
 
-    // Retorna o sorvete associado a este item.
     public IceCream getIceCream() {
         return iceCream;
     }
 
-    // Retorna a quantidade deste item.
     public int getQuantity() {
         return quantity;
     }
 
-    // Calcula o subtotal para este item do pedido.
     public double getSubtotal() {
         return iceCream.getBasePrice() * quantity;
     }
 
-    // Representação em string do item do pedido.
     @Override
     public String toString() {
         return quantity + "x " + iceCream.getName() + " ($" + String.format("%.2f", iceCream.getBasePrice()) + " each) - Subtotal: $" + String.format("%.2f", getSubtotal());
